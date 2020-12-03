e7547a8bfbc2f89e7a9f111254b99ccace97882b
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-03 22:39:20 +0100

    day3: compress code, just for fun

--- day03/main.py
+++ day03/main.py
@@ -3,16 +3,12 @@
 import math
 
 
-def count_trees(forest, dx, dy):
-    return sum([row[(i * dy) % len(forest[0])] == '#'
-                for i, row in enumerate(forest[::dx])])
+with open('input.txt') as f:
+    forest = f.read().splitlines()
 
+print('part 1:', sum([
+    row[(i * 3) % len(forest[0])] == '#' for i, row in enumerate(forest)]))
 
-if __name__ == '__main__':
-    slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
-
-    with open('input.txt') as f:
-        forest = f.read().splitlines()
-
-    print('part 1:', count_trees(forest, 1, 3))
-    print('part 2:', math.prod([count_trees(forest, *s) for s in slopes]))
+print('part 2:', math.prod([
+    sum([row[(i * dy) % len(forest[0])] == '#' for i, row in enumerate(forest[::dx])])
+    for dx, dy in [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]]))
