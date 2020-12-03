a82afd4412b0916626f9e4a5a0b24d33c2f04f12
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-03 07:40:42 +0100

    day3: move code to main block

--- day03/main.py
+++ day03/main.py
@@ -4,13 +4,10 @@
 
 import math
 
-with open('input.txt') as f:
-    forest = [l.rstrip() for l in f.readlines()]
-
 
 def count_trees(forest, slope):
     n, m = len(forest), len(forest[0])
-    i, j = 0, 0
+    i = j = 0
     trees = 0
 
     while (i < n):
@@ -21,7 +18,11 @@ def count_trees(forest, slope):
     return trees
 
 
-print('part 1:', count_trees(forest, (1, 3)))
-print('part 2:', math.prod(
-    [count_trees(forest, slope)
-     for slope in [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]]))
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        forest = [l.rstrip() for l in f.readlines()]
+
+    print('part 1:', count_trees(forest, (1, 3)))
+    print('part 2:', math.prod(
+        [count_trees(forest, slope)
+         for slope in [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]]))
