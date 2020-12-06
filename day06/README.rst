3e2f5f66fda631ec647e28a110d38c89ae56777d
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-06 07:32:10 +0100

    add day 6

---
+++ day06/README.rst
@@ -0,0 +1,5 @@
+=====================
+Day 6: Custom Customs
+=====================
+
+Count appearances of symbols in groups.
---
+++ day06/main.py
@@ -0,0 +1,20 @@
+"""Day 6: Custom Customs"""
+
+from collections import Counter
+
+
+def count_any_answered(group):
+    return len(Counter(group.replace('\n', '')))
+
+
+def count_all_answered(group):
+    return sum([count == len(group.split())
+                for _, count in Counter(group.replace('\n', '')).items()])
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        groups = f.read().split('\n\n')
+
+    print('part 1:', sum(map(count_any_answered, groups)))
+    print('part 2:', sum(map(count_all_answered, groups)))
