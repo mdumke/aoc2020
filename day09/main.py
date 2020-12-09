0baf1be0d9f1695f3fc6d5d07ecfb07d18143159
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-09 15:16:20 +0100

    day 9: refactor relying heavier on generators

--- day09/main.py
+++ day09/main.py
@@ -1,33 +1,34 @@
 """Day 9: Encoding Error"""
 
 from itertools import combinations
+from typing import Iterator, List
 
 PREAMBLE = 25
 
 
-def has_pair_summing_to(target, values):
-    return any((sum(pair) == target for pair in combinations(values, 2)))
-
-
-def sliding_window(values, width):
+def sliding_window(values, width) -> Iterator[List[int]]:
     return (values[i:i+width] for i in range(len(values)-width))
 
 
-def find_first_invalid(xmas):
-    return next(n for *preamble, n in sliding_window(xmas, PREAMBLE+1)
-                if not has_pair_summing_to(n, preamble))
+def has_pair_summing_to(target, values) -> bool:
+    return any((sum(pair) == target for pair in combinations(values, 2)))
 
 
-def find_encryption_weakness(xmas, target):
-    for i in range(len(xmas)):
-        cursor = i
-        batch = []
+def find_batch_summing_to(target, size, values) -> List[int]:
+    return next((batch
+                 for batch in sliding_window(values, size)
+                 if sum(batch) == target), None)
 
-        while sum(batch) < target:
-            batch.append(xmas[cursor])
-            cursor += 1
 
-        if sum(batch) == target:
+def find_first_invalid_number(xmas) -> int:
+    return next(number
+                for *preamble, number in sliding_window(xmas, PREAMBLE+1)
+                if not has_pair_summing_to(number, preamble))
+
+
+def find_encryption_weakness(xmas, invalid_number) -> int:
+    for batch_size in range(2, len(xmas)):
+        if (batch := find_batch_summing_to(invalid_number, batch_size, xmas)):
             return min(batch) + max(batch)
 
 
@@ -35,6 +36,6 @@ if __name__ == '__main__':
     with open('input.txt') as f:
         xmas = [int(n) for n in f.readlines()]
 
-    first_invalid = find_first_invalid(xmas)
+    first_invalid = find_first_invalid_number(xmas)
     print('part 1:', first_invalid)
     print('part 2:', find_encryption_weakness(xmas, first_invalid))
