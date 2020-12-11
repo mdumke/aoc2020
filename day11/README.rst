0d3de28ab3610a121a821245ad85d6ba903fb7a9
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-11 14:38:11 +0100

    day 11: refactor

---
+++ day11/README.rst
@@ -0,0 +1,5 @@
+======================
+Day 11: Seating System
+======================
+
+Evaluate neighboring and distant symbols on a grid. Read the `full problem description <https://adventofcode.com/2020/day/11>`_.
--- day11/main.py
+++ day11/main.py
@@ -1,155 +1,74 @@
+"""Day 11: Seating System"""
+
 from itertools import product
 
-def count_adjacent(i, j, seats):
-    count =0
-    for dx, dy in product([-1, 0, 1], repeat=2):
-        if (dx, dy) == (0, 0):
-            continue
-        if not (0 <= i + dx < len(seats)):
-            continue
-        if not (0 <= j + dy < len(seats[0])):
-            continue
 
-        count += seats[i+dx][j+dy] == '#'
-
-    return count
+def is_valid_position(x, y, list2d):
+    return 0 <= x < len(list2d) and 0 <= y < len(list2d[0])
 
 
-def update_seats(seats):
-    new_seats = []
-    n, m = len(seats), len(seats[0])
-
-    for row in range(n):
-        new_row = ''
-
-        for col in range(m):
-            if seats[row][col] == '.':
-                new_row += '.'
-            else:
-                adjacent = count_adjacent(row, col, seats)
-
-                if seats[row][col] == 'L' and adjacent == 0:
-                    new_row += '#'
-                elif seats[row][col] == '#' and adjacent >= 4:
-                    new_row += 'L'
-                else:
-                    new_row += seats[row][col]
-
-        new_seats.append(new_row)
-
-    return new_seats
+def checksum(pattern):
+    return hash(''.join([''.join(line) for line in pattern]))
 
 
-def find_final_seats(seats):
+def count(symbol, pattern):
+    return sum([s == symbol for row in pattern for s in row])
+
+
+def any_is_occupied(x, y, dx, dy, seats):
     while True:
-        new_seats = update_seats(seats)
+        x, y = x + dx, y + dy
 
-        if ''.join(seats) == ''.join(new_seats):
-            return new_seats
+        if not is_valid_position(x, y, seats) or seats[x][y] == 'L':
+            return False
 
-        seats = new_seats
-
-
-def count_occupied(seats):
-    return sum([s == '#' for row in seats for s in row])
-
-def zero_matrix(n, m):
-    return [[0 for _ in range(m)] for _ in range(n)]
+        if seats[x][y] == '#':
+            return True
 
 
-def is_valid_position(x, y, seats):
-    return (0 <= x < len(seats)) and (0 <= y < len(seats[0]))
-
-def rotate(matrix):
-    return list(list(l[::-1]) for l in zip(*matrix))
-
-def checksum(seats):
-    return hash(''.join([''.join(l) for l in seats]))
-
-def diagonal_trace(seats, counts) -> None:
-    def diagonal(x, y):
-        while is_valid_position(x, y, seats):
-            yield (x, y)
-            x, y = x + 1, y + 1
-
-    for row in range(len(seats)):
-        last_seat = '.'
-
-        for i, j in diagonal(row, 0):
-            counts[i][j] += last_seat == '#'
-
-            if seats[i][j] != '.':
-                last_seat = seats[i][j]
-
-    for col in range(1, len(seats[0])):
-        last_seat = '.'
-
-        for i, j in diagonal(0, col):
-            counts[i][j] += last_seat == '#'
-
-            if seats[i][j] != '.':
-                last_seat = seats[i][j]
+def adjacent_is_occupied(x, y, dx, dy, seats):
+    return is_valid_position(x+dx, y+dy, seats) and seats[x+dx][y+dy] == '#'
 
 
-def horizontal_trace(seats, counts) -> None:
-    for i, row in enumerate(seats):
-        last_seat = '.'
-
-        for j, current in enumerate(row):
-            counts[i][j] += last_seat == '#'
-
-            if current != '.':
-                last_seat = seats[i][j]
+def count_neighbors(x, y, seats, counting_policy):
+    return sum([counting_policy(x, y, dx, dy, seats)
+                for dx, dy in product([-1, 0, 1], repeat=2) if (dx, dy) != (0, 0)])
 
 
-def part2(seats):
+def update_position(x, y, pattern, tolerance, counting_policy):
+    symbol = pattern[x][y]
+    num_neighbors = count_neighbors(x, y, pattern, counting_policy)
+
+    if num_neighbors == 0 and symbol == 'L':
+        symbol = '#'
+    elif num_neighbors >= tolerance and symbol == '#':
+        symbol = 'L'
+
+    return symbol
+
+
+def zeros_like(pattern):
+    return [[0 for _ in range(len(pattern[0]))] for _ in range(len(pattern))]
+
+
+def stabilize(pattern, tolerance, counting_policy):
     while True:
-        counts = zero_matrix(len(seats), len(seats[0]))
+        next_pattern = zeros_like(pattern)
 
-        horizontal_trace(seats, counts)
-        diagonal_trace(seats, counts)
+        for i in range(len(pattern)):
+            for j in range(len(pattern[0])):
+                next_pattern[i][j] = update_position(
+                    i, j, pattern, tolerance, counting_policy)
 
-        seats, counts = rotate(seats), rotate(counts)
-        horizontal_trace(seats, counts)
-        diagonal_trace(seats, counts)
+        if checksum(next_pattern) == checksum(pattern):
+            return pattern
 
-        seats, counts = rotate(seats), rotate(counts)
-        horizontal_trace(seats, counts)
-        diagonal_trace(seats, counts)
-
-        seats, counts = rotate(seats), rotate(counts)
-        horizontal_trace(seats, counts)
-        diagonal_trace(seats, counts)
-
-        seats, counts = rotate(seats), rotate(counts)
-        counts
-
-        new_seats = []
-
-        for i, row in enumerate(seats):
-            new_row = []
-
-            for j, seat in enumerate(row):
-                if seat == 'L' and counts[i][j] == 0:
-                    new_seat = '#'
-                elif seat == '#' and counts[i][j] >= 5:
-                    new_seat = 'L'
-                else:
-                    new_seat = seat
-
-                new_row.append(new_seat)
-
-            new_seats.append(new_row)
-
-        if checksum(seats) == checksum(new_seats):
-            return new_seats
-
-        seats = new_seats
+        pattern = next_pattern
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-            seats = f.read().splitlines()
+        seats = f.read().splitlines()
 
-    print('part 1:', count_occupied(find_final_seats(seats)))
-    print('part 2:', count_occupied(part2(seats)))
+    print('part 1:', count('#', stabilize(seats, 4, adjacent_is_occupied)))
+    print('part 2:', count('#', stabilize(seats, 5, any_is_occupied)))
