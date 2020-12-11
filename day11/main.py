7d4969b52797a16fc0f7c66f4f3c908202e3c412
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-11 14:50:38 +0100

    day 11: rename variable

--- day11/main.py
+++ day11/main.py
@@ -30,8 +30,8 @@ def adjacent_is_occupied(x, y, dx, dy, seats):
     return is_valid_position(x+dx, y+dy, seats) and seats[x+dx][y+dy] == '#'
 
 
-def count_neighbors(x, y, seats, counting_policy):
-    return sum([counting_policy(x, y, dx, dy, seats)
+def count_neighbors(x, y, pattern, counting_policy):
+    return sum([counting_policy(x, y, dx, dy, pattern)
                 for dx, dy in product([-1, 0, 1], repeat=2) if (dx, dy) != (0, 0)])
 
 
