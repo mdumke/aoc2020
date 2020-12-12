2580a9a1aa07b42e66a279f31000c88d4cd76bfa
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-12 08:36:37 +0100

    day 12: refactor part 1

--- day12/main.py
+++ day12/main.py
@@ -1,64 +1,48 @@
 import re
 
-with open('input.txt') as f:
-    instructions = [(op, int(n)) for op, n in re.findall('(\w)(\d+)', f.read())]
 
-def forward(n, orientation, position):
-    if orientation == 'N':
-        return (position[0], position[1] + n)
-    if orientation == 'S':
-        return (position[0], position[1] - n)
-    if orientation == 'E':
-        return (position[0] + n, position[1])
-    if orientation == 'W':
-        return (position[0] - n, position[1])
+def manhattan(x, y):
+    return abs(x) + abs(y)
 
-    raise Error('unknown orientation')
+
+def update_position(x, y, direction, n):
+    if direction == 'N':
+        return x, y + n
+    if direction == 'S':
+        return x, y - n
+    if direction == 'E':
+        return x + n, y
+    if direction == 'W':
+        return x - n, y
 
 
 def rotate(orientation, op, n):
-    left = dict(E='N', S='E', W='S', N='W')
-    right = dict(E='S', S='W', W='N', N='E')
-    flip = dict(E='W', W='E', N='S', S='N')
-
-    if n == 90:
-        if op == 'L':
-            return left[orientation]
-        if op == 'R':
-            return right[orientation]
-
-    if n == 180:
-        return flip[orientation]
-
-    if n == 270:
-        if op == 'R':
-            return left[orientation]
-        if op == 'L':
-            return right[orientation]
-
-    raise Error('unknown orientation', op, n, orientation)
+    current = 'NESW'.find(orientation)
+    direction = 1 if op == 'R' else -1
+    return 'NESW'[(current + (n // 90) * direction) % 4]
 
 
-position = (0, 0)
-orientation = 'E'
-
-for op, n in instructions:
+def move(x, y, orientation, op, n):
     if op == 'F':
-        position = forward(n, orientation, position)
-    elif op == 'L':
-        orientation = rotate(orientation, op, n)
-    elif op == 'R':
-        orientation = rotate(orientation, op, n)
-    elif op == 'N':
-        position = (position[0], position[1] + n)
-    elif op == 'S':
-        position = (position[0], position[1] - n)
-    elif op == 'E':
-        position = (position[0] + n, position[1])
-    elif op == 'W':
-        position = (position[0] - n, position[1])
-    else:
-        raise Error('unknwon op')
+        return update_position(x, y, orientation, n), orientation
+    if op in 'NSEW':
+        return update_position(x, y, op, n), orientation
+    if op in 'LR':
+        return (x, y), rotate(orientation, op, n)
 
 
-print('part 1:', abs(position[0]) + abs(position[1]))
+def compute_destination_distance(instructions):
+    position = (0, 0)
+    orientation = 'E'
+
+    for op, n in instructions:
+        position, orientation = move(*position, orientation, op, n)
+
+    return manhattan(*position)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        ops = [(op, int(n)) for op, n in re.findall(r'(\w)(\d+)', f.read())]
+
+    print('part 1:', compute_destination_distance(ops))
