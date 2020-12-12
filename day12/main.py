985d564aac57a0721e954246461e7718dcf74c0b
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-12 15:28:02 +0100

    day 12: rename functions

--- day12/main.py
+++ day12/main.py
@@ -1,7 +1,7 @@
 """Day 12: Rain Risk"""
 
 
-def translate_coord(x, y, direction, n):
+def translate(x, y, direction, n):
     return {
         'N': (x, y + n),
         'S': (x, y - n),
@@ -10,7 +10,7 @@ def translate_coord(x, y, direction, n):
     }[direction]
 
 
-def rotate_coord(x, y, op, n):
+def rotate(x, y, op, n):
     return {
         90: (y, -x),
         180: (-x, -y),
@@ -27,29 +27,29 @@ def turn(direction, side, degrees):
     }[direction][(degrees if side == 'R' else 360 - degrees) // 90]
 
 
-def get_distance_part1(actions):
+def get_distance_strategy1(actions):
     x, y = 0, 0
     direction = 'E'
     for op, n in actions:
         if op == 'F':
-            x, y = translate_coord(x, y, direction, n)
+            x, y = translate(x, y, direction, n)
         if op in 'NSEW':
-            x, y = translate_coord(x, y, op, n)
+            x, y = translate(x, y, op, n)
         if op in 'LR':
             direction = turn(direction, op, n)
     return abs(x) + abs(y)
 
 
-def get_distance_part2(actions):
+def get_distance_strategy2(actions):
     x, y = 0, 0
     waypoint = 10, 1
     for op, n in actions:
         if op == 'F':
             x, y = x + waypoint[0] * n, y + waypoint[1] * n
         if op in 'NSEW':
-            waypoint = translate_coord(*waypoint, op, n)
+            waypoint = translate(*waypoint, op, n)
         if op in 'LR':
-            waypoint = rotate_coord(*waypoint, op, n)
+            waypoint = rotate(*waypoint, op, n)
     return abs(x) + abs(y)
 
 
@@ -57,5 +57,5 @@ if __name__ == '__main__':
     with open('input.txt') as f:
         actions = [(l[0], int(l[1:])) for l in f.read().splitlines()]
 
-    print('part 1:', get_distance_part1(actions))
-    print('part 2:', get_distance_part2(actions))
+    print('part 1:', get_distance_strategy1(actions))
+    print('part 2:', get_distance_strategy2(actions))
