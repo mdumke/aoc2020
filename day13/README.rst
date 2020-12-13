e0aaa1cf2e7bf8ee0cf9f5900501e8d218e847a0
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-13 16:43:57 +0100

    add day 13

---
+++ day13/README.rst
@@ -0,0 +1,5 @@
+======================
+Day 13: Shuttle Search
+======================
+
+Find target pattern in a number of prime numbers. Read the `full problem description <https://adventofcode.com/2020/day/13>`_.
---
+++ day13/main.py
@@ -0,0 +1,37 @@
+"""Day 13: Shuttle Search"""
+
+import math
+import numpy as np
+
+
+def get_next_bus(buses, start):
+    time, id = min([(bus - start % bus, i)
+                    for i, bus in enumerate(buses) if bus != 'x'])
+    return buses[id], time
+
+
+def first_match(start, step, target, offset):
+    i = 0
+    while i < target:
+        if (start + (i * step) + offset) % target == 0:
+            return start + i * step, step * target
+        i += 1
+
+
+def find_earliest_target_pattern(buses):
+    n = rep = buses[0]
+    for offset, bus in enumerate(buses[1:], 1):
+        if bus != 'x':
+            n, rep = first_match(n, rep, bus, offset)
+    return n
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        start = int(next(f))
+        buses = [int(n) if n.isdigit()
+                 else n for n in f.read().strip().split(',')]
+
+
+print('part 1:', math.prod(get_next_bus(buses, start)))
+print('part 2:', find_earliest_target_pattern(buses))
