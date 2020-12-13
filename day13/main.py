67937f8e23f1d0200d47d45954c21ede346d05e8
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-13 17:05:05 +0100

    day 13: use for loop and rename variables

--- day13/main.py
+++ day13/main.py
@@ -1,35 +1,31 @@
 """Day 13: Shuttle Search"""
 
 import math
-import numpy as np
 
 
 def get_next_bus(buses, start):
     return min([(bus - start % bus, bus) for bus in buses if bus != 'x'])
 
 
-def first_match(start, step, target, offset):
-    i = 0
-    while i < target:
-        if (start + (i * step) + offset) % target == 0:
-            return start + i * step, step * target
-        i += 1
+def first_match(time, cycle, target, offset):
+    for i in range(target):
+        if (time + i * cycle + offset) % target == 0:
+            return time + i * cycle, cycle * target
 
 
 def find_earliest_target_pattern(buses):
-    n = rep = buses[0]
+    time = cycle = buses[0]
     for offset, bus in enumerate(buses[1:], 1):
         if bus != 'x':
-            n, rep = first_match(n, rep, bus, offset)
-    return n
+            time, cycle = first_match(time, cycle, bus, offset)
+    return time
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         start = int(next(f))
-        buses = [int(n) if n.isdigit()
-                 else n for n in f.read().strip().split(',')]
+        buses = [int(n) if n.isdigit() else n
+                 for n in next(f).strip().split(',')]
 
-
-print('part 1:', math.prod(get_next_bus(buses, start)))
-print('part 2:', find_earliest_target_pattern(buses))
+    print('part 1:', math.prod(get_next_bus(buses, start)))
+    print('part 2:', find_earliest_target_pattern(buses))
