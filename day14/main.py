cab9398d6bfc631d3b9c12f08e1e83b1966e0d95
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-14 08:54:59 +0100

    day 14: refactor

--- day14/main.py
+++ day14/main.py
@@ -3,6 +3,47 @@
 import re
 from itertools import zip_longest, product
 
+
+def mask_value(val, mask):
+    return int(''.join([digit if m == 'X' else m
+                        for m, digit in zip_longest(mask[::-1], val[::-1], fillvalue='0')][::-1]), 2)
+
+
+def process_part1(prog):
+    mask = ''
+    memory = {}
+    for op, val in prog:
+        if op == 'mask':
+            mask = val
+        else:
+            memory[op] = mask_value(val, mask)
+    return sum(memory.values())
+
+
+def decode_address(addr, mask):
+    addr = bin(addr)[2:]
+    floating = [i for i, char in enumerate(mask) if char == 'X']
+    masked_addr = [m if m in ['1', 'X'] else digit
+                   for m, digit in zip_longest(mask[::-1], addr[::-1], fillvalue='0')][::-1]
+
+    for bits in product(['0', '1'], repeat=len(floating)):
+        for index, bit in zip(floating, bits):
+            masked_addr[index] = bit
+        yield int(''.join(masked_addr), 2)
+
+
+def process_part2(prog):
+    mask = ''
+    memory = {}
+    for op, val in prog:
+        if op == 'mask':
+            mask = val
+        else:
+            for address in decode_address(op, mask):
+                memory[address] = int(val, 2)
+    return sum(memory.values())
+
+
 def parse_line(l):
     op, val = l.split(' = ')
     if op == 'mask':
@@ -10,42 +51,6 @@ def parse_line(l):
     else:
         return int(re.findall(r'(\d+)', op)[0]), bin(int(val))[2:]
 
-
-def process_part1(prog):
-    mask = ''
-    memory = {}
-
-    for op, val in prog:
-        if op == 'mask':
-            mask = val
-        else:
-            memory[op] = int(''.join(reversed([digit if m == 'X' else m for m, digit in zip_longest(mask[::-1], val[::-1], fillvalue='0')])), base=2)
-
-    return sum(memory.values())
-
-def decode_address(addr, mask):
-    floating = [i for i, char in enumerate(mask) if char == 'X']
-    masked_addr = [m if m in ['1', 'X'] else digit
-            for m, digit in zip_longest(mask[::-1], bin(addr)[2:][::-1], fillvalue='0')][::-1]
-
-    for bits in product(['0', '1'], repeat=len(floating)):
-        for position, bit in zip(floating, bits):
-            masked_addr[int(position)] = bit
-        yield ''.join(masked_addr)
-
-
-def process_part2(prog):
-    mask = ''
-    memory = {}
-
-    for op, val in prog:
-        if op == 'mask':
-            mask = val
-        else:
-            for address in decode_address(op, mask):
-                memory[int(address, 2)] = int(val, 2)
-
-    return sum(memory.values())
 
 if __name__ == '__main__':
     with open('input.txt') as f:
