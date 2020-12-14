2e585f9781383e0152f5b90e4e179d6fbf8654a2
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-14 08:36:23 +0100

    add day 14, part 2

---
+++ day14/README.rst
@@ -0,0 +1,5 @@
+====================
+Day 14: Docking Data
+====================
+
+Apply a bitmask for value manipulation and as memory address decoder. Read the `full problem description <https://adventofcode.com/2020/day/14>`_.
--- day14/main.py
+++ day14/main.py
@@ -1,5 +1,7 @@
+"""Day 14: Docking Data"""
+
 import re
-from itertools import zip_longest
+from itertools import zip_longest, product
 
 def parse_line(l):
     op, val = l.split(' = ')
@@ -8,16 +10,46 @@ def parse_line(l):
     else:
         return int(re.findall(r'(\d+)', op)[0]), bin(int(val))[2:]
 
-with open('input.txt') as f:
-    prog = [parse_line(l) for l in f.read().splitlines()]
 
-mask = ''
-memory = {}
+def process_part1(prog):
+    mask = ''
+    memory = {}
 
-for op, val in prog:
-    if op == 'mask':
-        mask = val
-    else:
-        memory[op] = int(''.join(reversed([digit if m == 'X' else m for m, digit in zip_longest(mask[::-1], val[::-1], fillvalue='0')])), base=2)
+    for op, val in prog:
+        if op == 'mask':
+            mask = val
+        else:
+            memory[op] = int(''.join(reversed([digit if m == 'X' else m for m, digit in zip_longest(mask[::-1], val[::-1], fillvalue='0')])), base=2)
 
-print('part 1:', sum(memory.values()))
+    return sum(memory.values())
+
+def decode_address(addr, mask):
+    floating = [i for i, char in enumerate(mask) if char == 'X']
+    masked_addr = [m if m in ['1', 'X'] else digit
+            for m, digit in zip_longest(mask[::-1], bin(addr)[2:][::-1], fillvalue='0')][::-1]
+
+    for bits in product(['0', '1'], repeat=len(floating)):
+        for position, bit in zip(floating, bits):
+            masked_addr[int(position)] = bit
+        yield ''.join(masked_addr)
+
+
+def process_part2(prog):
+    mask = ''
+    memory = {}
+
+    for op, val in prog:
+        if op == 'mask':
+            mask = val
+        else:
+            for address in decode_address(op, mask):
+                memory[int(address, 2)] = int(val, 2)
+
+    return sum(memory.values())
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        prog = [parse_line(l) for l in f.read().splitlines()]
+
+    print('part 1:', process_part1(prog))
+    print('part 2:', process_part2(prog))
