140103e2dc8c47d86bf1601c6222ce60c69f98ec
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-17 11:39:45 +0100

    day 17: rename function

--- day17/main.py
+++ day17/main.py
@@ -11,8 +11,8 @@ def shift_coord(coord, offset):
     return tuple(dim + offset for dim in coord)
 
 
-def expand(dimensions, width):
-    return tuple(np.array(dimensions) + [width] * len(dimensions))
+def expand_dimensions(dims, width=2):
+    return tuple(np.array(dims) + [width] * len(dims))
 
 
 def iterate_coords(shape, padding=0):
@@ -28,7 +28,6 @@ def iterate_coords(shape, padding=0):
 def get_neighbors(grid):
     for coord in iterate_coords(grid.shape, padding=1):
         neighbors_idxs = tuple([slice(max(0, dim-1), dim+2) for dim in coord])
-
         if is_valid_coord(coord, grid.shape):
             store = grid[coord]
             grid[coord] = 0
@@ -38,22 +37,18 @@ def get_neighbors(grid):
         else:
             neighbors = grid[neighbors_idxs].copy()
             is_active = 0
-
         yield coord, is_active, neighbors
 
 
 def evolve(grid, generations=1):
     for _ in range(generations):
-        new_grid = np.zeros(expand(grid.shape, 2))
-
+        new_grid = np.zeros(expand_dimensions(grid.shape))
         for coord, is_active, neighbors in get_neighbors(grid):
             if is_active:
                 new_grid[shift_coord(coord, 1)] = neighbors.sum() in [2, 3]
             else:
                 new_grid[shift_coord(coord, 1)] = neighbors.sum() == 3
-
         grid = new_grid
-
     return grid
 
 
