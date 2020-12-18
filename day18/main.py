3bcaad1eaf5823f83094e53722aea4304014338e
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-18 10:37:39 +0100

    day 18: refactor

--- day18/main.py
+++ day18/main.py
@@ -3,24 +3,7 @@
 from operator import add, mul
 
 
-def next_available(formula):
-    depth = 0
-    candidate = None
-    for i, symbol in enumerate(reversed(formula)):
-        if symbol == '(':
-            depth += 1
-        if symbol == ')':
-            depth -= 1
-        if symbol == '+' and depth == 0:
-            candidate = add, len(formula) - i - 1
-            break
-        if symbol == '*' and depth == 0:
-            candidate = mul, len(formula) - i - 1
-            break
-    return candidate
-
-
-def lowest_precedence(formula):
+def get_operator(formula, advanced):
     depth = 0
     candidate = None
     for i, symbol in enumerate(reversed(formula)):
@@ -33,24 +16,26 @@ def lowest_precedence(formula):
             break
         if symbol == '+' and depth == 0:
             candidate = add, len(formula) - i - 1
+            if not advanced:
+                break
     return candidate
 
 
-def evaluate(formula, get_operator):
+def evaluate(formula, advanced=False):
     if formula.isdigit():
         return int(formula)
 
-    if op := get_operator(formula):
-        left = evaluate(formula[:op[1]], get_operator)
-        right = evaluate(formula[op[1]+1:], get_operator)
-        return op[0](left, right)
-    else:
-        return evaluate(formula[1:-1], get_operator)
+    if op := get_operator(formula, advanced):
+        return op[0](
+            evaluate(formula[:op[1]], advanced),
+            evaluate(formula[op[1]+1:], advanced))
+
+    return evaluate(formula[1:-1], advanced)
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         formulas = [l.replace(' ', '') for l in f.read().splitlines()]
 
-    print('part 1:', sum(evaluate(f, next_available) for f in formulas))
-    print('part 2:', sum(evaluate(f, lowest_precedence) for f in formulas))
+    print('part 1:', sum(evaluate(f) for f in formulas))
+    print('part 2:', sum(evaluate(f, True) for f in formulas))
