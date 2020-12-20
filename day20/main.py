6ad8e34721a207a2876b95eb4cd5c5e3aa0cd89e
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-20 17:56:21 +0100

    day 20: refactor image class

--- day20/main.py
+++ day20/main.py
@@ -6,35 +6,46 @@ from collections import defaultdict
 
 TOP, RIGHT, BOTTOM, LEFT = 0, 1, 2, 3
 
+
 class Image:
-    def __init__(self, raw):
-        self.id = int(re.split(r'[ :]', raw[0])[1])
-        self.data = np.array([list(l) for l in raw[1:]])
+    def __init__(self, img_id, data):
+        self.id = img_id
+        self.data = data
+
+    @staticmethod
+    def from_input(lines):
+        return Image(
+            int(re.split(r'[ :]', lines[0])[1]),
+            np.array([list(l) for l in lines[1:]]))
 
     @property
     def borders(self):
-        # top, right, bottom left
+        # top, right, bottom, left
         return [
             ''.join(self.data[0, :]),
             ''.join(self.data[:, -1]),
             ''.join(self.data[-1, :]),
             ''.join(self.data[:, 0])]
 
-    def transform(self, border, target_position):
+    def variations(self):
+        data = self.data.copy()
         for _ in range(4):
-            if self.borders[target_position] == border:
-                return
-            self.data = np.rot90(self.data)
-
-        self.data = np.flip(self.data, axis=0)
-
+            data = np.rot90(data)
+            yield Image(self.id, data)
+        data = np.flip(data, axis=0)
         for _ in range(4):
-            if self.borders[target_position] == border:
-                return
-            self.data = np.rot90(self.data)
+            data = np.rot90(data)
+            yield Image(self.id, data)
+
+    def algin_borders(self, border, target_position):
+        for img in self.variations():
+            if img.borders[target_position] == border:
+                return img
+
 
 def get_adjacent_positions(x, y, img):
     return zip([(x, y+1), (x+1, y), (x, y-1), (x-1, y)], zip(img.borders, [BOTTOM, LEFT, TOP, RIGHT]))
+
 
 def build_lookup_by_border(images):
     lookup = defaultdict(list)
@@ -56,6 +67,7 @@ def to_matrix(img_lookup):
 
     return matrix
 
+
 def build_grid(start_img, images):
     available_images = build_lookup_by_border(images)
 
@@ -68,16 +80,18 @@ def build_grid(start_img, images):
     while open_positions:
         (x, y), (border, border_position) = open_positions.pop()
 
-        neighbors = [img for img in available_images.get(border, []) if img.id not in processed]
+        neighbors = [img for img in available_images.get(
+            border, []) if img.id not in processed]
 
         if not neighbors:
-            neighbors = [img for img in available_images.get(border[::-1], []) if img.id not in processed]
+            neighbors = [img for img in available_images.get(
+                border[::-1], []) if img.id not in processed]
 
         if not neighbors:
             continue
 
         neighbor = neighbors[0]
-        neighbor.transform(border, border_position)
+        neighbor = neighbor.algin_borders(border, border_position)
         processed.add(neighbor.id)
         grid[(x, y)] = neighbor
         open_positions.extend(get_adjacent_positions(x, y, neighbor))
@@ -86,8 +100,9 @@ def build_grid(start_img, images):
 
 
 def to_image(tile_matrix):
-    return np.concatenate([np.concatenate([img.data[1:-1,1:-1] for img in row], axis=1)
-                          for row in tile_matrix], axis=0)
+    return np.concatenate([np.concatenate([img.data[1:-1, 1:-1] for img in row], axis=1)
+                           for row in tile_matrix], axis=0)
+
 
 def mark_monsters(image):
     monster = np.array([
@@ -100,14 +115,16 @@ def mark_monsters(image):
 
     for x in range(image.shape[0] - monster.shape[0]):
         for y in range(image.shape[1] - monster.shape[1]):
-            n_matches = sum(image[i, j] == '#' for i, j in monster_idxs + (x, y))
-            if  n_matches == len(monster_idxs):
+            n_matches = sum(image[i, j] == '#' for i,
+                            j in monster_idxs + (x, y))
+            if n_matches == len(monster_idxs):
                 n_monsters += 1
 
                 for i, j in monster_idxs + (x, y):
                     image[i, j] = 'O'
 
     return image, n_monsters
+
 
 def count_hashes_without_monster(image):
     img = image.copy()
@@ -130,15 +147,15 @@ def count_hashes_without_monster(image):
 
         img = np.rot90(img)
 
+
 if __name__ == '__main__':
     with open('input.txt') as f:
-        image_data = [l.splitlines() for l in f.read().split('\n\n')]
-        images = [Image(line) for line in image_data]
+        images = [Image.from_input(lines.splitlines())
+                  for lines in f.read().split('\n\n')]
 
     grid = build_grid(images[0], images)
-    print('part 1:', grid[0][0].id * grid[0][-1].id * grid[-1][0].id * grid[-1][-1].id)
+    print('part 1:', grid[0][0].id * grid[0]
+          [-1].id * grid[-1][0].id * grid[-1][-1].id)
 
     final = to_image(grid)
     print('part 2:', count_hashes_without_monster(final))
-
-
