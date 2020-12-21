b050c94362c11128f1a26f4c50dc89c5c79850ac
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-21 14:17:06 +0100

    day 21: add part 2

---
+++ day21/README.rst
@@ -0,0 +1,5 @@
+===========================
+Day 21: Allergen Assessment
+===========================
+
+Determine possible assignments of allergnes to ingredients in a list of foods. Read the `full problem description <https://adventofcode.com/2020/day/21>`_.
--- day21/main.py
+++ day21/main.py
@@ -1,26 +1,51 @@
-def count_ingredients_without_allergenes(foods):
-    candidates = {}
+"""Day 21: Allergen Assessment"""
 
+from operator import itemgetter
+from itertools import permutations
+
+
+def find_possible_ingredients(foods):
+    """returns lookup {allergene: {ingredients}}"""
+    candidates = {}
     for ingredients, allergenes in foods:
         for a in allergenes:
-            if candidates.get(a):
-                candidates[a] = candidates[a] & set(ingredients)
-            else:
+            if not candidates.get(a):
                 candidates[a] = set(ingredients)
+            candidates[a] = candidates[a] & set(ingredients)
+    return candidates
 
-    combined = set()
 
-    for ingredients in candidates.values():
-        combined |= ingredients
+def combine_sets(sets):
+    """returns the union of given sets"""
+    return sets[0].union(*sets[1:])
 
-    allergene_free = 0
 
-    for ingredients, _ in foods:
-        for i in ingredients:
-            if i not in combined:
-                allergene_free += 1
+def count_ingredients_without_allergenes(foods):
+    """returns number of ingredients that cannot contain known allergenes"""
+    candidates = find_possible_ingredients(foods)
+    return sum([ing not in combine_sets(list(candidates.values()))
+                for ingredients, _ in foods for ing in ingredients])
 
-    return allergene_free
+
+def find_allergene_assignment(foods):
+    """returns list of [(allergene, ingredient)]"""
+    candidates = find_possible_ingredients(foods)
+    combined = combine_sets(list(candidates.values()))
+
+    for assignment in permutations(list(combined), r=len(candidates)):
+        valid = True
+        for allergene, ingredient in zip(candidates, assignment):
+            if ingredient not in candidates[allergene]:
+                valid = False
+                break
+        if valid:
+            return list(zip(candidates, assignment))
+
+
+def get_dangerous_ingredients(foods):
+    """returns list of ingredients"""
+    assignment = find_allergene_assignment(foods)
+    return ','.join([i for a, i in sorted(assignment, key=itemgetter(0))])
 
 
 if __name__ == '__main__':
@@ -29,3 +54,4 @@ if __name__ == '__main__':
                   for part in line[:-2].split(' (contains ')] for line in f]
 
     print('part 1:', count_ingredients_without_allergenes(foods))
+    print('part 2:', get_dangerous_ingredients(foods))
