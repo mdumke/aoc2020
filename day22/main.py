820344e308548c831d8bf4e596704447ccbe2bdd
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-22 10:22:48 +0100

    day 22: refactor

--- day22/main.py
+++ day22/main.py
@@ -2,8 +2,9 @@
 
 
 class Deck(list):
-    def copy(self):
-        return Deck(super(Deck, self).copy())
+    @property
+    def score(self):
+        return sum([i * n for i, n in enumerate(reversed(self), 1)])
 
     def __getitem__(self, idx):
         return Deck(super(Deck, self).__getitem__(idx))
@@ -15,18 +16,15 @@ class Deck(list):
 def combat(deck1, deck2):
     while deck1 and deck2:
         card1, card2 = deck1.pop(0), deck2.pop(0)
-
         if card1 > card2:
             deck1.extend([card1, card2])
         else:
             deck2.extend([card2, card1])
-
     return deck1, deck2
 
 
 def recursive_combat(deck1, deck2):
-    cache1 = set()
-    cache2 = set()
+    cache1, cache2 = set(), set()
 
     while True:
         if deck1 in cache1 and deck2 in cache2:
@@ -37,20 +35,15 @@ def recursive_combat(deck1, deck2):
 
         cache1.add(deck1)
         cache2.add(deck2)
-
-        card1 = deck1.pop(0)
-        card2 = deck2.pop(0)
+        card1, card2 = deck1.pop(0), deck2.pop(0)
 
         if len(deck1) >= card1 and len(deck2) >= card2:
-            p1, _ = recursive_combat(
-                deck1[:card1].copy(),
-                deck2[:card2].copy())
+            sub1 = recursive_combat(deck1[:card1], deck2[:card2])[0]
+            winner = deck1 if sub1 else deck2
+        else:
+            winner = deck1 if card1 > card2 else deck2
 
-            if p1:
-                deck1.extend([card1, card2])
-            else:
-                deck2.extend([card2, card1])
-        elif card1 > card2:
+        if winner == deck1:
             deck1.extend([card1, card2])
         else:
             deck2.extend([card2, card1])
@@ -69,8 +62,8 @@ if __name__ == '__main__':
     player1 = load_deck('player1.txt')
     player2 = load_deck('player2.txt')
 
-    p1, p2 = combat(player1.copy(), player2.copy())
-    print('part 1:', score(p1 if p1 else p2))
+    p1, p2 = combat(player1[:], player2[:])
+    print('part 1:', p1.score if p1 else p2.score)
 
-    p1, p2 = recursive_combat(player1.copy(), player2.copy())
-    print('part 2:', score(p1 if p1 else p2))
+    p1, p2 = recursive_combat(player1[:], player2[:])
+    print('part 2:', p1.score if p1 else p2.score)
