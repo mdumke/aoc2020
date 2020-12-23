fc43f187fe4576f880e04b65725bcffd5f3fc79e
Matthias Dumke <matthias.dumke@gmx.net>
2020-12-23 10:51:57 +0100

    day 23: add part 2

--- day23/main.py
+++ day23/main.py
@@ -1,12 +1,15 @@
+"""Day 23: Crab Cups"""
+
+import math
 from more_itertools import pairwise
 
 def serialize(cups, start):
     done = set()
-    result = ''
+    result = []
     current = start
 
     while current not in done:
-        result += str(current)
+        result.append(current)
         done.add(current)
         current = cups[current]
 
@@ -40,10 +43,26 @@ def play_game(numbers):
     for i in range(100):
         current = move(cups, current, max_n)
 
-    return serialize(cups, 1)[1:]
+    return ''.join(map(str, serialize(cups, 1)[1:]))
+
+
+def play_advanced_game(numbers):
+    numbers = [*numbers, *list(range(max(numbers)+1, 1000001))]
+    cups = dict(pairwise(numbers))
+    cups[numbers[-1]] = numbers[0]
+
+    current = numbers[0]
+    max_n = max(numbers)
+
+    for i in range(10_000_000):
+        current = move(cups, current, max_n)
+
+    return serialize(cups, 1)[1:3]
 
 
 if __name__ == '__main__':
     numbers = [2, 4, 7, 8, 1, 9, 3, 5, 6]
 
     print('part 1:', play_game(numbers))
+    print('part 2:', math.prod(play_advanced_game(numbers)))
+
